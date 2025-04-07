57bcb1b702007e559ef5985b3d544d15f141b227
Richard Doe <doerichard190@gmail.com>
2025-04-07 22:27:59 +0200

    Final 1.0.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
+  const states = [];
+  const currentState = { ...state };
+
   // write code here
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        Object.assign(currentState, obj.extraData);
+        states.push({ ...currentState });
+        break;
+      case 'removeProperties':
+        for (const toDelete of obj.keysToRemove) {
+          delete currentState[toDelete];
+        }
+        states.push({ ...currentState });
+        break;
+      case 'clear':
+        Object.keys(currentState).forEach((key) => delete currentState[key]);
+        states.push({ ...currentState });
+        break;
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
